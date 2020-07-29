1a810efe1ed900e26612d525da84f5613f0ff6d2
Michael Place <mplace@wisc.edu>
2020-07-29 16:26:31 -0500

    lift over documentation from original pascal code, compiles

--- src/dalvec.c
+++ src/dalvec.c
@@ -1,132 +1,119 @@
 /* Output from p2c 2.00.Oct.15, the Pascal-to-C translator */
 /* From input file "dalvec.p" */
+/* 
+dalvec: converts Rseq rsdata file to symvec format
+   by Thomas Schneider
+module libraries required: delman, prgmods
 
+name
+   dalvec: converts Rseq rsdata file to symvec format
 
- #include "/root/src/p2c-2.01/home/src/p2c.h"
+synopsis
+   dalvec(rsdata: in, dalvecp: in, symvec: out, output: out)
 
+files
+   rsdata: data file from rseq program
 
-/*
+   dalvecp: parameters to control dalvec
+      If empty, then the normal sequence logo will be produced.
+      If the first character of the first line is a 'c', then a chi-logo
+      is produced.  The height of this logo is the information.  The
+      heights of the individual letters are, however, not the frequencies,
+      but rather their partial chi-square values.  The expected value
+      is 1/4 of the number of characters.  This is compared to the observed
+      value by:
+        partial chi-square =(observed - expected)^2/expected
+      These partial values are normalized and placed in symvec in place of
+      the relative frequencies.  Thus the significance of each letter is
+      used.  When the observed is less than expected, the reported value
+      is made negative.  Makelogo prints these characters upside down.
+   symvec: reformating of the rsdata file for input to the makelogo program.
+      A series of header lines begining with asterisk ("*") are produced.
+      The next line contains one integer which is the number of symbols
+      in the vector (4 for DNA or RNA, 20 for proteins).
+      After this, the format of the file is a series of entries.  Each entry
+      has two parts.  The first part is on one line and contains
+         position total information
+         position: the position number
+         total:  the sum of the values in the vector
+         information: the information content of the vector.
+      The remaining parameters on the line are from the rsdata file:
+         rs: sum of rsl
+         varhnb: variance of rsl
+         sumvar: sum of varhnb
+         ehnb: 2-e(n)
+
+      The second part consists of a list of a series of symbol lines.  The
+      number of these matches the numer of symbols (4 in the case of DNA),
+      representing the the numbers of bases or amino acids at the position in
+      an aligned set of sequences.  Each line begins with the character of the
+      symbol, followed by the number of that symbols.
+
+   output: messages to the user
+
+description
+   Convert the rsdata file from rseq into a format that the makelogo program
+   can use.  The format is a 'symbol vector'.
+
+   ChiLogos: If you leave the parameter file empty, then the standard sequence
+   logo will be created.  However, if the first letter of the file is a 'c',
+   then a new kind of logo will emerge from makelogo: the chi-logo.  The height
+   is as it was before.  The height of the individual letters is different,
+   instead of being proportional to the frequency of the letter, it is
+   proportional to the significance of the letter, based on the chi-square
+   test.  That is, the expected number of letters is the number of letters at
+   that position, n(l) divided by 4 (for simplicity!).  The observed number
+   comes from the rsdata file.  The partial-chi square is
+   (observed-expected)^2/expected.  Note that the sum of the partials is the
+   normal chi-square.  So bases that contribute strongly get big.  Also, bases
+   that are under represented are printed UPSIDE DOWN, so you can (usually)
+   tell you have a chilogo at a glance.  The chilogo allows one to see the
+   importance of the infrequent letters.  The technical mechanism for making a
+   letter upside down is to have its number negative in the symvec file.
+
+examples
+
+see also
+   rseq.p, makelogo.p
+
+author
+   Thomas D. Schneider
+
+bugs
+
+   The program originally only created a vector that contained the characters
+   of the alphabet, so the output was called an 'alvec'.  To reflect the use of
+   symbols, the name of the output file was changed to symvec, but I like
+   'dalvec', and 'dsymvec' is so awkward that I decided to keep the name
+   dalvec.
+
+To Compile:
+
+gcc dalvec.c -o dalvec -I/home/mplace/bin/p2c/src -L /home/mplace/bin/p2c/src -lm -lp2c
+
+To Run:
+
+dalvec  -c cmp -e encseq 
 
 */
 
 
+#include <getopt.h>  /* getopt API */ 
+#include <stdio.h>   /* printf */
+#include <stdlib.h> 
+#include </home/mplace/bin/p2c/src/p2c.h>
 
 #define version         2.20
-/*
-*/
-
-
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-
-
 #define infofield       8
 #define infodecim       5
-
 #define nfield          4
 #define mapmax          26
-
 #define pwid            8
 #define pdec            5
-
 #define pnum            100000L
-
 #define cxmove          30
-
-
-
 #define maxstring       150
-
-
-
 #define fillermax       21
-
-
-
 
 typedef struct rstype {
   long rstart, rstop, l, nal, ncl, ngl, ntl;
@@ -136,14 +123,10 @@ typedef struct rstype {
   Char flag;
 } rstype;
 
-
-
 typedef struct stringDelila {
   Char letters[maxstring];
   long length, current;
 } stringDelila;
-
-
 
 typedef struct trigger {
   stringDelila seek;
@@ -151,37 +134,15 @@ typedef struct trigger {
   boolean skip, found;
 } trigger;
 
-
-
-/*
-
-
-*/
 typedef Char filler[fillermax];
-
-
-
 Static _TEXT rsdata, dalvecp, symvec;
-
-
 Static jmp_buf _JL1;
-
-
 
 Static Void halt()
 {
-  /*
-
-
-
-
-
-*/
   printf(" program halt.\n");
   longjmp(_JL1, 1);
 }
-
-
 
 Static Void clearstring(ribbon)
 stringDelila *ribbon;
@@ -194,8 +155,6 @@ stringDelila *ribbon;
   ribbon->current = 0;
 }
 
-
-
 Static Void writestring(tofile, s)
 _TEXT *tofile;
 stringDelila *s;
@@ -207,15 +166,6 @@ stringDelila *s;
     putc(s->letters[i], tofile->f);
 }
 
-
-
-/*
-
-
-
-
-*/
-
 Static Void resettrigger(t)
 trigger *t;
 {
@@ -224,23 +174,12 @@ trigger *t;
   t->found = false;
 }
 
-
 Static Void testfortrigger(ch, t)
 Char ch;
 trigger *t;
 {
-  /*
-
-
-
-
-*/
   t->state++;
-  /*
 
-
-
-*/
   if (t->seek.letters[t->state - 1] == ch) {
     t->skip = false;
     if (t->state == t->seek.length)
@@ -254,24 +193,14 @@ trigger *t;
   t->found = false;
 }
 
-
-
 Static Void fillstring(s, a)
 stringDelila *s;
 Char *a;
 {
-  /*
-*/
-
-
-  /*
-
-
-*/
   long length = fillermax;
   long index;
-
   clearstring(s);
+
   while (length > 1 && a[length-1] == ' ')
     length--;
   if (length == 1 && a[length-1] == ' ') {
@@ -285,16 +214,12 @@ Char *a;
   s->current = 1;
 }
 
-
-
 Static Void filltrigger(t, a)
 trigger *t;
 Char *a;
 {
   fillstring(&t->seek, a);
 }
-
-
 
 Static Void copyaline(fin, fout)
 _TEXT *fin, *fout;
@@ -308,15 +233,9 @@ _TEXT *fin, *fout;
   putc('\n', fout->f);
 }
 
-
-
-
-
 Static Void header(infile, outfile)
 _TEXT *infile, *outfile;
 {
-  /*
-*/
   long index;
 
   if (*infile->name != '\0') {
@@ -355,17 +274,10 @@ _TEXT *infile, *outfile;
 	  "* position, number of sequences, information Rs, variance of Rs\n");
 }
 
-
-
-
-
-
 Static Void readrsrange(rsdata, r)
 _TEXT *rsdata;
 rstype *r;
 {
-  /*
-*/
   long index;
   Char skip;
 
@@ -380,17 +292,11 @@ rstype *r;
     skip = ' ';
 }
 
-
-
 Static Void getrsbegin(infile)
 _TEXT *infile;
 {
-  /*
-*/
   Char ch;
   trigger begindata;
-
-
   filltrigger(&begindata, "l    a    c    g    t");
   resettrigger(&begindata);
 
@@ -422,8 +328,6 @@ _TEXT *infile;
   getc(infile->f);
 }
 
-
-
 Static Void readrsdata(rsdata, rdata)
 _TEXT *rsdata;
 rstype *rdata;
@@ -437,16 +341,10 @@ rstype *rdata;
     getc(rsdata->f);
   fscanf(rsdata->f, "%c%*[^\n]", &rdata->flag);
   getc(rsdata->f);
-  /*
-*/
+
   if (rdata->flag == '\n')
     rdata->flag = ' ';
 }
-
-
-
-
-
 
 Static double sign(a)
 double a;
@@ -457,10 +355,7 @@ double a;
     return -1.0;
 }
 
-
 #define bignumber       pnum
-
-
 
 Static Void themain(rsdata, dalvecp, symvec)
 _TEXT *rsdata, *dalvecp, *symvec;
@@ -532,7 +427,6 @@ _TEXT *rsdata, *dalvecp, *symvec;
   header(rsdata, symvec);
   fprintf(symvec->f, "4 number of symbols in DNA or RNA\n");
 
-
   if (*rsdata->name != '\0') {
     if (rsdata->f != NULL)
       rsdata->f = freopen(rsdata->name, "r", rsdata->f);
@@ -545,21 +439,14 @@ _TEXT *rsdata, *dalvecp, *symvec;
   RESETBUF(rsdata->f, Char);
   readrsrange(rsdata, &rdata);
   getrsbegin(rsdata);
-
-
   getrsbegin(rsdata);
-
-
   FORLIM = rdata.rstop;
+
   for (position = rdata.rstart; position <= FORLIM; position++) {
     if (P_peek(rsdata->f) != '*') {
       readrsdata(rsdata, &rdata);
 
-
       if (chilogo) {
-	/*
-
-*/
 	nl4 = rdata.nl / 4.0;
 	TEMP = rdata.nal - nl4;
 	chia = TEMP * TEMP;
@@ -589,31 +476,20 @@ _TEXT *rsdata, *dalvecp, *symvec;
 	}
       }
 
-      /*
-
-
-*/
-
       fprintf(symvec->f, "%*ld %*ld %*.*f % .*E\n",
 	      nfield, rdata.l, infofield, rdata.nl, infofield, infodecim,
 	      rdata.rsl, P_max((int)(infofield + 3) - 7, 1), rdata.varhnb);
-
       fprintf(symvec->f, "a %*ld\n", nfield, rdata.nal);
       fprintf(symvec->f, "c %*ld\n", nfield, rdata.ncl);
       fprintf(symvec->f, "g %*ld\n", nfield, rdata.ngl);
       fprintf(symvec->f, "t %*ld\n", nfield, rdata.ntl);
-
     }
   }
-
 }
 
 #undef bignumber
 
-
-main(argc, argv)
-int argc;
-Char *argv[];
+int main(int argc, Char **argv)
 {
   PASCAL_MAIN(argc, argv);
   if (setjmp(_JL1))
@@ -633,8 +509,5 @@ _L1:
   if (symvec.f != NULL)
     fclose(symvec.f);
   exit(EXIT_SUCCESS);
-}
-
-
-
-/* End. */
+  return 0;
+}/* End. */
